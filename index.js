7eefd380ff119313110d76e6bf6b8d4feaa07049
altair720230 <54368159+altair720230@users.noreply.github.com>
2024-01-23 15:21:04 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Sincronizando Antepasados","Explorando Atalayas","Desincronizando"];
+const statusMessages = ["Sincronizando Antepasados","Explorando Atalayas","Desincronizando Antepasado"];
 
 
 let currentIndex = 0;
